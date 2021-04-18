7aa5a626b765d90303a03722bfc5708bff17d85a
Manoj <manoj7pal@gmail.com>
2021-04-18 10:10:02 -0400

    Added - Non Local Keyword examples

--- 7_Nested Function_NonLocal_Closure_Decorator.py
+++ 7_Nested Function_NonLocal_Closure_Decorator.py
@@ -35,4 +35,53 @@ def outer():
     print(f"num: {num}")
     # print(f"num2: {num2}") # Outer() cannot access inner() variables/properties.
 
+
 # outer()
+
+# Non Local Keyword: used to modify the state of Outer function properties
+# ---------------------------------------------------------------------------
+# global
+num = 100
+print(f"global num: {num}")
+print('--' * 30)
+
+
+def outer():
+    print("Inside Outer()")
+    # local
+    num = 200
+    print(f"local num: {num}")
+    print('--' * 30)
+
+    def inner1():
+        print("Inside INNER1")
+        num = 300
+        print(f"local num: {num}")
+        print('--' * 30)
+
+    def inner2():
+        print("Inside INNER2")
+        global num
+        num = 400
+        print(f"global num: {num}")
+        print('--' * 30)
+
+    def inner3():
+        print("Inside INNER3")
+        nonlocal num
+        num = 500
+        print(f"nonlocal num: {num}")
+        print('--' * 30)
+
+    inner1()
+    inner2()
+    inner3()
+
+    print(f"local num: {num}")
+
+
+outer()
+print(f"global num: {num}")
+print('--' * 30)
+
+
