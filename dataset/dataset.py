f87d28b344454b0e286e317d9a434d6810df7880
Manoj <manoj7pal@gmail.com>
2021-05-15 17:32:50 -0400

    Added utility functions to load datasets

---
+++ dataset/dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+url_file = "C:/MANOJ/Academic/SelfStudy/Python/USF_Python/Sunbeam/dataset/url.csv"
+
+
+def list_datasets():
+    # print(os.getcwd())
+
+    datasets = pd.read_csv(url_file, names=['name', 'url'])
+    print('--' * 40)
+    print('Avaialbale Datasets:')
+    print('--' * 40)
+    for dataset in datasets.name.values:
+        print(dataset.strip())
+
+
+def get_url(dataset_name):
+    datasets = pd.read_csv(url_file, names=['name', 'url'])
+    dataset_url = datasets[datasets.name == dataset_name].url.values[0].strip()
+    return dataset_url
